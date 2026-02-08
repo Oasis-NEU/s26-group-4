8e4520d613fe5bce31f41fe0334e7424f7fb7c64
mcheng1359 <cheng.matt1@northeastern.edu>
2026-02-08 13:05:46 -0500

    added basic grid (very bad)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +43,33 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div >
+        <BasicGrid />
+      </div>
     </>
   )
+}
+
+export function BasicGrid() {
+  return (
+    //flex grow resizes cells by default if not fixed
+    <Box sx={{ flexGrow: 1 }}> 
+      <Grid container spacing={2}>
+        <Grid size={4}>
+          <Item>size=8 lakshdoiuas duihaiud hauishd iuauidhaiushd uiahiuhauishd uhauidhuia dsjai isabdia</Item>
+        </Grid>
+        <Grid size={4}>
+          <Item>size=4</Item>
+        </Grid>
+        <Grid size={4}>
+          <Item>size=4</Item>
+        </Grid>
+        <Grid size={4}>
+          <Item>size=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default App
