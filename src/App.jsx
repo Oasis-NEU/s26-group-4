b6e850f26006f9f8cce5a98f3c76291af0eec300
Arslan Ayushin <ayushin.a@northeastern.edu>
2026-03-06 23:07:37 -0500

    calendar shows correct month and days

--- src/App.jsx
+++ src/App.jsx
@@ -23,33 +23,62 @@ function App() {
 
   return (
     <>
-      <div >
-        <MonthGrid day={2} />
+      <div className="calendar">
+        <MonthGrid day={2}/>
       </div>
     </>
   )
 }
 
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
 function MonthGrid(props) {
   let day = props.day;
+  const date = new Date();
+  const currentMonth = date.getMonth();
+  const currentDay = date.getDate();
+  const currentYear = date.getFullYear();
+
+  const leapYear = currentYear % 400 == 0 || (currentYear % 4 == 0 && currentYear % 100 != 0);
+  const janFirstOffset = (currentYear % 100 + Math.floor(currentYear % 100/4)) % 7
+    + (leapYear ? -1 : 0);
+  const monthOffsets = [0,3,3,6,1,4,6,2,5,0,3,5];
+  const currentMonthOffset = monthOffsets[currentMonth] + janFirstOffset;
+  const monthDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+  const currentMonthDays = monthDays[currentMonth] + (leapYear && currentMonth == 1 ? 1 : 0);
+  const prevMonth = mod(currentMonth - 1, 12);
+  const prevMonthDays = monthDays[prevMonth] + (leapYear && prevMonth == 1 ? 1 : 0);
+  const monthNames = ['January','February','March','April','May','June','July',
+    'August','September','October','November','December'];
+
   return (
     //flex grow resizes cells by default if not fixed
-    <Box sx={{ flexGrow: 1}}> 
+    <Box sx={{ flexGrow: 1}}>
+      <div>
+        {monthNames[currentMonth]}
+      </div>
       <Grid container spacing={0.5} columns={7}>
-        {Array.from(Array(30)).map((_, index) => (
-          <MonthCell day={index + 1}/>
+        {Array.from(Array(42)).map((_, index) => (
+          index >= currentMonthOffset
+          ? <MonthCell day={mod((index-currentMonthOffset), currentMonthDays) + 1}/>
+          : <MonthCell day={mod((index-currentMonthOffset), prevMonthDays) + 1}/>
         ))}
       </Grid>
     </Box>
   );
-  function MonthCell(props){
-    let day = props.day;
-    return (
-      <Grid size={1}>
-        <Item>{day}</Item>
-      </Grid> 
-    );
-  }
+}
+
+function MonthCell(props){
+  let day = props.day;
+  return (
+    <Grid size={1}>
+      <Item onClick={() => {
+        alert('hello');
+      }}>{day}</Item>
+    </Grid>
+  );
 }
 
 export default App
