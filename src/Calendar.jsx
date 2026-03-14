184b7b79f05fee277d7f91eff17147ac12721aa2
mcheng1359 <mcheng1359@bths.edu>
2026-03-13 21:03:26 -0400

    added missing function from refactor

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import MonthGrid from './MonthGrid';
+import { incrementMonth, decrementMonth } from './MonthGrid'
 import WeekGrid from './WeekGrid';
 
 function Calendar() {
