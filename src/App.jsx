cbd47a15b1c9ff06256bf83880b995a9c5d55336
mcheng1359 <mcheng1359@bths.edu>
2026-03-21 23:34:01 -0400

    added working rendition of time picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Calendar from './Calendar.jsx';
+import TimePicker from './TimePicker.jsx'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Calendar/>
+      <TimePicker/>
     </>
   )
 }
